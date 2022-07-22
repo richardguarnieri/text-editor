a3bf1c345bd99a336f1f046c4a8437d685802419
Richard Guarnieri <richard.gm@outlook.com>
2022-07-22 14:20:00 -0500

    small project configuration

--- server/server.js
+++ server/server.js
@@ -1,12 +1,16 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 app.use(express.static('../client/dist'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+});
 
-app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
+console.log(__dirname)
+app.listen(port, () => console.log(`Now listening on port: ${port}`));
