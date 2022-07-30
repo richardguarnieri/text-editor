32baeefce8f22ded87da7653ef66c38726deacd9
Richard Guarnieri <richard.gm@outlook.com>
2022-07-30 00:44:45 -0400

    fixed IDB put method - added id to arguments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
-  console.log('Put to the database');
+  console.log('PUT to the database');
 
   // Create a connection to the database database and version we want to use.
   const jateDb = await openDB('jate', 1);
@@ -24,9 +24,8 @@ export const putDb = async (id, content) => {
 
   // Open up the desired object store.
   const store = tx.objectStore('jate');
-
   // Use the .put() method on the store and pass in the content.
-  const request = store.put({id: id, content: content});
+  const request = store.put({ id: id, content: content });
 
   // Get confirmation of the request.
   const result = await request;
@@ -52,7 +51,6 @@ export const getDb = async () => {
   // Get confirmation of the request.
   const result = await request;
   console.log('result.value', result);
-  return result;
 };
 
 initdb();
