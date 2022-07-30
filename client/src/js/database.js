bbc47be12247124cb591d8489e6e61d27624b333
Richard Guarnieri <richard.gm@outlook.com>
2022-07-30 09:44:25 -0400

    cleaned JS comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Added logic to a method that accepts some content and adds it to the database
 export const putDb = async (id, content) => {
   console.log('PUT to the database');
 
@@ -32,7 +32,7 @@ export const putDb = async (id, content) => {
   console.log('🚀 - data saved to the database', result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// Added logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET from the database');
 
